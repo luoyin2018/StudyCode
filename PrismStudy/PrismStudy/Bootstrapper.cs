9abff282521e40523eee22a2c02e19ddf24048dd
luoyin <luoyin2018@163.com>
2018-01-17 11:28:42 +0800

    Prism step 1: 添加包引用，添加Bootstrapper, 编辑App Startup逻辑

---
+++ PrismStudy/PrismStudy/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace PrismStudy
+{
+    class Bootstrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
