d05f0b96874a8bb647000daa1875145a6ff7a3db
luoyin <luoyin2018@163.com>
2018-01-25 16:53:03 +0800

    尝试使用地图控件 1) 微软的MapControl:权限问题如何解决？帮助文档比较少 2) GMap.Net可以用，但是地图拼接问题该如何解决？

---
+++ ChartControlStudy/UseGMap/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GMap.NET; 
+using GMap.NET.MapProviders;
+
+namespace UseGMap
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Map.CacheLocation = Environment.CurrentDirectory;
+            this.Map.MapProvider   = BaiduMapProvider.Instance;
+            this.Map.Manager.Mode  = AccessMode.ServerAndCache;
+            this.Map.MinZoom       = 1;
+            this.Map.MaxZoom       = 23;
+            this.Map.Zoom          = 15;
+            this.Map.ShowCenter    = false;
+            this.Map.Position      = new PointLatLng(21,67.88);
+        }
+    }
+}
